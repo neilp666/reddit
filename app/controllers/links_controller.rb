42299a25e62fc32e43028d476c626005ebaa2826
neil patel <neil@myindianweddingapp.com>
2015-01-03 11:03:19 +0000

    Authorisation on links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   # GET /links
   # GET /links.json
@@ -14,7 +15,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +25,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
@@ -62,6 +63,11 @@ class LinksController < ApplicationController
   end
 
   private
+
+    def authorised_user
+      @link = current_user.links.find_by(id: params[:id])
+      redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
