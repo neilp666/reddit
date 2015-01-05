cf3587810d722784c380d7476c6bbb780d77d1d6
neil patel <neil@myindianweddingapp.com>
2015-01-05 19:22:46 +0000

    Add Comments

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'devise', '~> 3.4.1'
 gem 'bootstrap-sass', '~> 3.2.0.2'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'simple_form', '~>3.0.2'
 gem 'jbuilder', '~> 2.0'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,60 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @comments = Comment.all
+    respond_with(@comments)
+  end
+
+  def show
+    respond_with(@comment)
+  end
+
+  def new
+    @comment = Comment.new
+    respond_with(@comment)
+  end
+
+  def edit
+  end
+
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+
+  respond_to do |format|
+    if @comment.save
+        format.html { redirect_to @link, notice: 'Comment was successfully created.'}
+        format.json { render json: @comment, status: :created, location: @comment }
+    else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+    end
+   end
+  end
+
+  def update
+    @comment.update(comment_params)
+    respond_with(@comment)
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed'}
+      format.json { head :no_content}
+    end
+  end
+
+  private
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:link_id, :body, :user_id)
+    end
+end
